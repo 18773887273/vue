98c274b4e59c87e27359015b71973a8e7fc9a311
18773887273 <2113249768@qq.com>
2020-12-09 09:39:04 +0800

    前端 12.9 9:24

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <div style="height:200px">
+    <div style="height:200px; width: 200px">
     <el-form ref="form" label-width="80px">
+      <el-form-item>
+        <h1>用户登录</h1>
+      </el-form-item>
       <el-form-item label="用户名">
         <el-input v-model="username"></el-input>
       </el-form-item>
